f8eb2847eeb3e1df2dd30f6a3c523eb8201b510a
Shad-islam <shadislam48@gmail.com>
2024-01-09 01:33:52 +0600

    Create navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import "./App.css";
+import { ReactComponent as Logo } from "./images/logo.svg";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <div className="logo"></div>
-        <div className="menu"></div>
-        <div className="menu-icon"></div>
-      </nav>
+      <div className="conatiner">
+        <nav>
+          <div className="nav-left">
+            <div className="logo">
+              <Logo />
+            </div>
+            <div className="menu">
+              <ul>
+                <li> Features</li>
+                <li> Pricing</li>
+                <li> Resources</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="nav-right">
+            <a href="#" className="login-button">
+              Log In
+            </a>
+            <button className="signup-button">Sign Up</button>
+            <div className="menu-icon">
+              <GiHamburgerMenu />
+            </div>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }
