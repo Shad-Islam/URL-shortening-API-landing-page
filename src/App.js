ed23467d248c328659b6a49591b784fc84745215
Shad-islam <shadislam48@gmail.com>
2024-01-10 05:32:27 +0600

    Make navbar as a separate component

--- src/App.js
+++ src/App.js
@@ -1,65 +1,12 @@
-import React from "react";
-import { useState } from "react";
-import {
-  GlobalStyles,
-  Container,
-  Navbar,
-  NavLeft,
-  Menu,
-  MenuList,
-  MenuItems,
-  Item,
-  NavRight,
-  LoginButton,
-  SignupButton,
-  MenuIcon,
-} from "./components/styles/global.style"; // Replace with the correct file path
-import { ReactComponent as Logo } from "./images/logo.svg";
-import { GiHamburgerMenu } from "react-icons/gi";
+import NavbarSection from "./components/navbar";
+import { GlobalStyles, Container } from "./components/styles/global.style";
 
 function App() {
-  const [isMenuVisible, setIsMenuVisible] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuVisible(!isMenuVisible);
-  };
-
   return (
     <>
       <GlobalStyles />
       <Container>
-        <Navbar>
-          <NavLeft>
-            <div className="logo">
-              {/* Assuming Logo is just an image */}
-              <Logo />
-            </div>
-            <Menu isMenuVisible={isMenuVisible}>
-              <MenuList isMenuVisible={isMenuVisible}>
-                <MenuItems>
-                  <Item href="#">Features</Item>
-                </MenuItems>
-                <MenuItems>
-                  <Item href="#">Pricing</Item>
-                </MenuItems>
-                <MenuItems>
-                  <Item href="#">Resources</Item>
-                </MenuItems>
-              </MenuList>
-            </Menu>
-          </NavLeft>
-
-          <NavRight>
-            <LoginButton href="#" className="login-button">
-              Log In
-            </LoginButton>
-            <SignupButton className="signup-button">Sign Up</SignupButton>
-            <MenuIcon id="menu-icon" onClick={toggleMenu}>
-              {/* Assuming GiHamburgerMenu is an SVG icon */}
-              <GiHamburgerMenu />
-            </MenuIcon>
-          </NavRight>
-        </Navbar>
+        <NavbarSection />
       </Container>
     </>
   );
---
+++ src/components/navbar.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useState } from "react";
+import {
+  Menu,
+  Item,
+  Navbar,
+  NavLeft,
+  NavRight,
+  MenuIcon,
+  MenuList,
+  MenuItems,
+  LoginButton,
+  SignupButton,
+} from "./styles/nav.style";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { ReactComponent as Logo } from "../images/logo.svg";
+
+function NavbarSection() {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuVisible(!isMenuVisible);
+  };
+  return (
+    <>
+      <Navbar>
+        <NavLeft>
+          <div className="logo">
+            {/* Assuming Logo is just an image */}
+            <Logo />
+          </div>
+          <Menu isMenuVisible={isMenuVisible}>
+            <MenuList isMenuVisible={isMenuVisible}>
+              <MenuItems>
+                <Item href="#">Features</Item>
+              </MenuItems>
+              <MenuItems>
+                <Item href="#">Pricing</Item>
+              </MenuItems>
+              <MenuItems>
+                <Item href="#">Resources</Item>
+              </MenuItems>
+            </MenuList>
+          </Menu>
+        </NavLeft>
+
+        <NavRight>
+          <LoginButton href="#" className="login-button">
+            Log In
+          </LoginButton>
+          <SignupButton className="signup-button">Sign Up</SignupButton>
+          <MenuIcon id="menu-icon" onClick={toggleMenu}>
+            {/* Assuming GiHamburgerMenu is an SVG icon */}
+            <GiHamburgerMenu />
+          </MenuIcon>
+        </NavRight>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavbarSection;
