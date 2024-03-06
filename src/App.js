27137b4c396a0eed1ba6ec66c79b6440705eabf5
Shad-islam <m.shadislam01@gmail.com>
2024-03-06 23:14:58 +0600

    Solved banner issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Search from "./components/Search";
-import Banner from "./components/banner";
+import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Feature from "./components/Feature";
 import NavbarSection from "./components/Navbar";
@@ -17,9 +17,9 @@ function App() {
           setIsMenuVisible={setIsMenuVisible}
         />
         <Banner isMenuVisible={isMenuVisible} />
-         <Search />
+        <Search />
         <Feature />
-        <Footer /> 
+        <Footer />
       </Container>
     </>
   );
